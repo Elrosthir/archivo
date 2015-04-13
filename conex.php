419e25fa795f63236953146212124e63cc1d8b7d
Elrosthir <elros_tasartir@hotmail.es>
2015-04-13 17:24:02 +0200

    Create conex.php

---
+++ conex.php
@@ -0,0 +1,7 @@
+<?php
+	$link = mysql_connect('localhost', 'root', '')
+    or die('No se pudo conectar: ' . mysql_error());
+
+	mysql_select_db('uned') or die('No se pudo seleccionar la base de datos');
+	@mysql_query('SET NAMES "utf8"');
+?>
