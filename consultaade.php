405e53ccbafb9cddfea6c4dd2d332943c9d241e8
Elrosthir <elros_tasartir@hotmail.es>
2015-04-13 17:23:48 +0200

    Create consultaade.php

---
+++ consultaade.php
@@ -0,0 +1,15 @@
+<?php
+include('conex.php');
+
+$dia=$_POST['dia'];
+$query = "SELECT * FROM tutorias WHERE curso='ade' AND dia='$dia' ORDER BY ano";
+$result = mysql_query($query) or die('Consulta 	fallida: ' . mysql_error());	
+
+$arrDatos = array();
+while ($rs=mysql_fetch_array($result))
+{
+        $arrDatos[] = array_map('utf8_encode', $rs);
+}
+echo json_encode($arrDatos);
+@mysql_query('SET NAMES "utf8"');
+?>  
