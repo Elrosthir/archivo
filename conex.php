fca7b050a5e75676b664be5a27312c0f9c8cb01f
Elrosthir <elros_tasartir@hotmail.es>
2015-04-15 13:32:24 +0200

    Delete conex.php

--- conex.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$link = mysql_connect('192.168.1.200', 'root', '')
-    or die('No se pudo conectar: ' . mysql_error());
-
-	mysql_select_db('uned') or die('No se pudo seleccionar la base de datos');
-	@mysql_query('SET NAMES "utf8"');
-?>
