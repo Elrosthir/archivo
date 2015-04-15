a1278d2cff30fae57d2fdbbc3ee2d0495e37abfb
Elrosthir <elros_tasartir@hotmail.es>
2015-04-15 11:03:10 +0200

    Update conex.php

--- conex.php
+++ conex.php
@@ -1,5 +1,5 @@
 <?php
-	$link = mysql_connect('localhost', 'root', '')
+	$link = mysql_connect('192.168.1.200', 'root', '')
     or die('No se pudo conectar: ' . mysql_error());
 
 	mysql_select_db('uned') or die('No se pudo seleccionar la base de datos');
